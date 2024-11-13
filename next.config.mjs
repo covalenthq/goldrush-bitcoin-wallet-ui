3322575c3fe3dbfeae320e60a33466d8f16db8d1
Karan Pargal <karanpargal007@gmail.com>
2024-11-13 20:11:58 +0530

    fix: Allow image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    swcMinify: false,
+  swcMinify: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
